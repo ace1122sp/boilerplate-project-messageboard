f2fd7b15dd2c858fa295cead2a7e53bebb1d23f6
ace11 <ace11.space22@gmail.com>
2019-03-19 14:51:00 +0100

    ace init

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'client.bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+      { 
+        test: /\.scss$/,  
+        use: [
+          "style-loader",
+          "sass-loader",
+          "postcss-loader"
+        ]
+      }
+    ]
+  }
+};
