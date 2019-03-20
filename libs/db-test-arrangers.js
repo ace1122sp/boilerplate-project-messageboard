04023f86998a7a6912840031fe41a3afb7481c64
ace11 <ace11.space22@gmail.com>
2019-03-20 18:35:20 +0100

    server libs: structure for some db test arrange methods

---
+++ libs/db-test-arrangers.js
@@ -0,0 +1,15 @@
+const createThread = (done, thread) => {
+  // create thread 
+  // update board with created thread
+}; 
+
+const deleteThread = (done, threadId = null) => {
+  // if first param is omitted, then method should delete first thread it finds
+  // delete thread
+  // remove its ref from board
+};
+
+module.exports = {
+  createThread,
+  deleteThread
+};
