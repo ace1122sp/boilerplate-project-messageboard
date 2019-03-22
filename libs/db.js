0dc8932a57a827596e0a9ffba743f8a5a572da4d
ace11 <ace11.space22@gmail.com>
2019-03-22 18:09:41 +0100

    server libs: implemented default board on db start

--- libs/db.js
+++ libs/db.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
+const Board = require('../models/Board');
 
 module.exports = dbURI => {
   mongoose.Promise = global.Promise;
   mongoose.connect(dbURI, { useNewUrlParser: true, useFindAndModify: false })
+    .then(() => Board.initDefault('general'))
     .then(() => {
       console.log('connected to db');
     })
-    .catch(err => {});
+    .catch(err => {
+      console.error(err); // temp solution for dev
+    });
 };
