ab8eb6c12a3824a065a56e924dcc160d388d151d
ace11 <ace11.space22@gmail.com>
2019-03-22 18:08:59 +0100

    model Board: added init method to create default board

--- models/Board.js
+++ models/Board.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const uuid = require('uuid/v4');
 
 const { BOARD_NAME_MIN_LENGTH, BOARD_NAME_MAX_LENGTH } = require('../config/constants');
  
@@ -21,6 +22,19 @@ const BoardSchema = new mongoose.Schema({
       ref: 'Thread'
     }]
   }
+});
+
+BoardSchema.static('initDefault', function(name) {
+  this.findOne({}, (err, res) => {
+    if (err) {
+      console.error(err); // temp for development
+      return Promise.reject(err);
+    } else if (res === null) {
+      return this.create({ _id: uuid(), name });
+    } else {
+      return Promise.resolve();
+    }
+  });
 });
 
 const Board = mongoose.model('Board', BoardSchema);
