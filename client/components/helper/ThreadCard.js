58ef2f07b718d8df675d40e4eed35602d11605ef
ace11 <ace11.space22@gmail.com>
2019-03-29 17:26:21 +0100

    client: updated ThreadCard component

--- client/components/helper/ThreadCard.js
+++ client/components/helper/ThreadCard.js
@@ -9,11 +9,11 @@ const ThreadCard = ({ thread }) => {
     <div>
       <div>
         <h3><Link to={`/b/general/${thread.text}`}>{thread.text}</Link></h3>
-        <ul>
-          <li><button>report</button></li>
-          <li><FontAwesomeIcon size='1x' icon='angle-down' /></li>
-          <li>x</li>
-        </ul>
+        <div>
+          <button>report</button>
+          <button><FontAwesomeIcon size='1x' icon='angle-down' /></button>
+          <button>x</button>
+        </div>
       </div>
       <section>
         <ul>
