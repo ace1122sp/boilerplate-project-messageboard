19beaabb8d86f0fb243cc3873c47735d0f17afee
ace11 <ace11.space22@gmail.com>
2019-03-29 17:23:06 +0100

    client: updated ThreadCard component

--- client/components/helper/ThreadCard.js
+++ client/components/helper/ThreadCard.js
@@ -2,27 +2,30 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link } from 'react-router-dom';
 
-const ThreadCard = ({ thread }) => 
-  <div>
+const ThreadCard = ({ thread }) => {
+  const replies = thread.replies.map(reply => <li key={reply._id}>{reply.text}</li>);
+  
+  return (
     <div>
-      <h3><Link to={`/b/general/${thread.text}`}>{thread.text}</Link></h3>
-      <ul>
-        <li><button>report</button></li>
-        <li><FontAwesomeIcon size='1x' icon='angle-down' /></li>
-        <li>x</li>
-      </ul>
+      <div>
+        <h3><Link to={`/b/general/${thread.text}`}>{thread.text}</Link></h3>
+        <ul>
+          <li><button>report</button></li>
+          <li><FontAwesomeIcon size='1x' icon='angle-down' /></li>
+          <li>x</li>
+        </ul>
+      </div>
+      <section>
+        <ul>
+          {replies}
+        </ul>      
+        <aside>
+          <h4>{replies.length} replies</h4>
+          <p>bumped on: {thread.bumped_on}</p>
+        </aside>
+      </section>
     </div>
-    <section>
-      <ul>
-        <li>reply</li>
-        <li>reply</li>
-        <li>reply</li>
-      </ul>      
-      <aside>
-        <h4># replies</h4>
-        <p>bumped on: ##</p>
-      </aside>
-    </section>
-  </div>
-
+  );
+}
+  
 export default ThreadCard;
