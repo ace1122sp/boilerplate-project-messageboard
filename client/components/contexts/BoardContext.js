85ecad92f06018399bad05efa9dca7703dea46f3
ace11 <ace11.space22@gmail.com>
2019-03-29 16:48:08 +0100

    client: added BoardContext context & EmptyBoard component

---
+++ client/components/contexts/BoardContext.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { DEFAULT_BOARD } from '../../constants';
+
+const BoardContext = React.createContext(DEFAULT_BOARD);
+
+export default BoardContext;
---
+++ client/components/helper/EmptyBoard.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const EmptyBoard = () => 
+  <div>
+    <h2>No Threads on the Board</h2>
+  </div>
+
+export default EmptyBoard;
