0be1c06dd66eea2f1c1c20f25e5712b0648f37a4
ace11 <ace11.space22@gmail.com>
2019-04-01 21:42:28 +0200

    client: AddThreadPanel component --> added basic functionality for closing panel and sending post request

--- client/components/helper/AddThreadPanel.js
+++ client/components/helper/AddThreadPanel.js
@@ -1,15 +1,59 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 
-const AddThreadPanel = () => 
-  <div>
-    <button>x</button>
-    <form>
+import { post } from '../../libs/apiHandler';
+import { threadURL } from '../../libs/urlHandler';
+
+import BoardContext from '../contexts/BoardContext';
+
+const AddThreadPanel = ({ close, addToThreads }) => {
+  const board = useContext(BoardContext);
+  const [sending, updateRequestStatus] = useState(false);
+  const [notification, setNotification] = useState(null);
+  const [thread, updateThread] = useState('');
+  const [delete_password, updatePassword] = useState('');
+
+  const addThread = e => {
+    e.preventDefault();
+    updateRequestStatus(true);
+    post(threadURL(board), { text: thread, delete_password })
+      .then(res => {
+        if (res._id) {
+          addToThreads(res);
+          setNotification('Thread added successfully!');
+        } else {
+          setNotification('Something went wrong!');
+        }
+        updateRequestStatus(false);
+      })
+      .catch(err => {});
+  };
+
+  const handleThreadChange = e => {
+    updateThread(e.target.value);
+  };
+
+  const handlePasswordChange = e => {
+    updatePassword(e.target.value);
+  };
+
+  const showForm = () => 
+    <form onSubmit={addThread}>
       <label>New Thread</label>
-      <input type='text' placeholder='thread' />
+      <input type='text' placeholder='thread' value={thread} onChange={handleThreadChange} />
       <label>password</label>
-      <input type='password' /> 
+      <input type='password' placeholder='password' value={delete_password} onChange={handlePasswordChange} /> 
       <button>submit</button>
     </form>
-  </div>
+
+  const showNotification = () => 
+    <h3>{notification}</h3>
+
+  return (
+    <div>
+      <button onClick={close}>x</button>
+      {(!notification && showForm()) || showNotification()}
+    </div>
+  );
+}
 
 export default AddThreadPanel;
--- client/components/main/Board.js
+++ client/components/main/Board.js
@@ -7,11 +7,13 @@ import { threadURL } from '../../libs/urlHandler';
 import BoardContext from '../contexts/BoardContext';
 import ThreadCard from '../helper/ThreadCard';
 import EmptyBoard from '../helper/EmptyBoard';
+import AddThreadPanel from '../helper/AddThreadPanel';
 
 const Board = () => {
   const board = useContext(BoardContext);
   const [threads, setThreads] = useState([]);
   const [loading, setLoadingStatus] = useState(true);
+  const [addThreadPanelOpened, toggleAddThreadPanel] = useState(false);
 
   const showThreadCards = threads.map(thread => <ThreadCard key={thread._id} thread={thread} apiUrl={threadURL(board)} />);
   
@@ -28,12 +30,25 @@ const Board = () => {
       .catch(err => {
         console.log(err) // temp solution
       });
-  }
+  };
+
+  const openAddThreadPanel = () => {
+    toggleAddThreadPanel(true);
+  };
+
+  const closeAddThreadPanel = () => {
+    toggleAddThreadPanel(false);
+  };
+
+  const addToThreads = thread => {
+    setThreads(() => [...threads, thread]);
+  };
 
   return (
     <main>
+      {addThreadPanelOpened && <AddThreadPanel close={closeAddThreadPanel} addToThreads={addToThreads} />}
       <div>
-        <button><FontAwesomeIcon size='1x' icon='plus' />add thread</button>
+        <button onClick={openAddThreadPanel}><FontAwesomeIcon size='1x' icon='plus' />add thread</button>
       </div>
       <section>
         {threads.length === 0 && <EmptyBoard />}
