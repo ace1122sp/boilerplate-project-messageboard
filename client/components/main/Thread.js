c924d76878755c7087d5ad3cd32648c74c059b0f
ace11 <ace11.space22@gmail.com>
2019-04-02 18:45:17 +0200

    client: Thread component -> added functionality to add & show new reply

--- client/components/main/Thread.js
+++ client/components/main/Thread.js
@@ -6,10 +6,13 @@ import { threadURL, replyURL } from '../../libs/urlHandler';
 
 import BoardContext from '../contexts/BoardContext';
 import ReplyCard from '../helper/ReplyCard';
+import PasswordPanel from '../helper/PasswordPanel';
 
 const Thread = ({ match }) => {
   const board = useContext(BoardContext);
   const [thread, setThread] = useState({ replies: [] });
+  const [newReply, updateNewReply] = useState('');
+  const [replyPasswordPanelOpened, toggleReplyPasswordPanel] = useState(false);
   const [loading, setLoadingStatus] = useState(true);
 
   const showReplyCards = thread.replies.map(reply => <li key={reply._id}><ReplyCard reply={reply} /></li>)
@@ -29,10 +32,57 @@ const Thread = ({ match }) => {
       .catch(err => {
         console.error(err); // temp solution
       });
+  };
+
+  const addToReplies = reply => {
+    setThread(() => {
+      const updatedReplies = [...thread.replies, reply];
+      return {...thread, replies: [...updatedReplies]};
+    });
   }
+
+  const handleReplyPost = delete_password => {
+    const data = {
+      thread_id: thread._id, 
+      text: newReply,
+      delete_password
+    };
+    resetReply();
+    post(replyURL(board), data)
+      .then(res => {
+        if (res._id) {
+          addToReplies(res);
+          console.log('reply added');
+        } else {
+          console.log('something went wrong');
+        }
+      })
+      .catch(error => {
+        console.error(err); // temp solution for development
+      });
+  };
+
+  const addReply = e => {
+    e.preventDefault();
+    toggleReplyPasswordPanel(true);
+  };
+
+  const handleReplyChange = e => {
+    updateNewReply(e.target.value);
+  };
+
+  const closeReplyPasswordPanel = () => {
+    toggleReplyPasswordPanel(false);
+  };
+
+  const resetReply = () => {
+    closeReplyPasswordPanel();
+    updateNewReply('');
+  };
 
   return (
     <main>
+      {replyPasswordPanelOpened && <PasswordPanel message='Enter Reply Password' handler={handleReplyPost} close={resetReply} />}
       <div>
         <h2>{thread.text}</h2>
         <div>
@@ -40,8 +90,8 @@ const Thread = ({ match }) => {
           <button><FontAwesomeIcon size='1x' icon='trash-alt' /></button>
         </div>        
       </div>
-      <form>
-        <input type='text' placeholder='reply' />
+      <form onSubmit={addReply}>
+        <input type='text' placeholder='reply' value={newReply} onChange={handleReplyChange} />
         <button>add reply</button>
       </form>
       <section>
