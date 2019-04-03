f9bf37fc12f399e5bb2c1d501b314fd777b647de
ace11 <ace11.space22@gmail.com>
2019-04-03 15:54:30 +0200

    client: ReplyCard component: mark reply as reported after you check it has not been deleted

--- client/components/helper/ReplyCard.js
+++ client/components/helper/ReplyCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ReplyCard = ({ thread, reply, report, setReplyToDelete }) => 
   <div>    
     <button onClick={() => setReplyToDelete(reply._id)}>x</button>
-    <p>{reply.reported ? 'reported' : reply.text}</p>
+    <p>{(reply.reported && reply.text !== '[deleted]') ? 'reported' : reply.text}</p>
     <div>
       <p>created on: <time>{reply.created_on}</time></p>
       <button onClick={() => report(thread, reply._id)}>report</button>
