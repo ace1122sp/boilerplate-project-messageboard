ed2ad7edbc376a6aa684f073aa77260927a865b5
ace11 <ace11.space22@gmail.com>
2019-04-11 17:58:23 +0200

    client: added center class to NotFound component

--- client/components/main/NotFound.js
+++ client/components/main/NotFound.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NotFound = () => 
   <main className='container main main-padding'>
-    <div className='center-row'>
+    <div className='center-row center'>
       <h2>404... Not Found!</h2>
     </div>
   </main>
